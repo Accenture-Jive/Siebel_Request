63263db9688a2ecbac7b25a801e07d2e50ba4a6d
Suresh Peddapu <suresh.peddapu@accenture.com>
2013-04-01 17:09:44 +0530

    commit changes

--- javascripts/jive.js
+++ javascripts/jive.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-alert("hello");	
+alert("hi");	
 	//Get the list of SRs assigned to the current user
 	var xmlDoc = ''; //XML String used for Parsing and like.
 	var srNoDisc = ''; //SR Number used for Discussions.
